0c829d79dbc58796689fbcc3ba34fbb8053d4c0b
cadenkulp22 <cadenkulp@gmail.com>
2023-06-16 09:08:57 -0700

    passed all tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [recipeFormShown, showRecipeForm] = useState(false);
+
+  let submitRecipe = (event) => {};
+
   return (
     <div className="App">
       <h1 className="App-header">My Recipes</h1>
       <p>There are no recipes to list.</p>
+      {
+        recipeFormShown ? 
+          <>
+            <form id="recipe-form" name='recipe-form' onSubmit={submitRecipe}>
+              <label htmlFor="newRecipeName">Recipe name: </label>
+              <input type="text" id="newRecipeName" />
+              <label htmlFor="newRecipeInstructions">Instructions:</label>
+              <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." />
+              <input type="submit" />
+            </form>
+          </>
+          : 
+            <button onClick={ () => showRecipeForm(!recipeFormShown) }>Add Recipe</button>
+      }
     </div>
   );
 }
